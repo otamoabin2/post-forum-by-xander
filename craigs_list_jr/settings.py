7721afc2c8aa31cecf61216c8649e76c74c89b5d
Xander Stevenson <xander@xanders-mbp.attlocal.net>
2019-03-23 18:59:15 -0500

    here we go

--- craigs_list_jr/settings.py
+++ craigs_list_jr/settings.py
@@ -120,16 +120,16 @@ STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = (
 #     os.path.join(PROJECT_ROOT, 'static'),
-)
-STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_DIRS=(
+# )
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_DIRS = (
     '‎⁨/iCloud Drive⁩/ Desktop⁩/ C_P⁩/ Assessments⁩/ assessment-4⁩/ craigs_list_jr⁩/ craigs_list_jr⁩/ static',
 )
 
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-prod_db=dj_database_url.config(conn_max_age = 500)
+prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
