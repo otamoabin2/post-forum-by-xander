f58cb18c5240f1b7ef91f24662c13d9eb34d27fd
Xander Stevenson <xander@xanders-mbp.attlocal.net>
2019-03-23 17:09:08 -0500

    here we go

--- craigs_list_jr/settings.py
+++ craigs_list_jr/settings.py
@@ -118,18 +118,18 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # STATICFILES_DIRS = (
-#     '‎⁨/iCloud Drive⁩/ Desktop⁩/ C_P⁩/ Assessments⁩/ assessment-4⁩/ craigs_list_jr⁩/ craigs_list_jr⁩/ static',
-# )
+#     os.path.join(PROJECT_ROOT, 'static'),
+)
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_DIRS=(
+    '‎⁨/iCloud Drive⁩/ Desktop⁩/ C_P⁩/ Assessments⁩/ assessment-4⁩/ craigs_list_jr⁩/ craigs_list_jr⁩/ static',
+)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
-prod_db = dj_database_url.config(conn_max_age=500)
+prod_db=dj_database_url.config(conn_max_age = 500)
 DATABASES['default'].update(prod_db)
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
